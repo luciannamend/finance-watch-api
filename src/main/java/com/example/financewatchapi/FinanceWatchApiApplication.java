eda27e8b37fe95c3a7ef277d0c87e0664615b53a
Lucianna Mendonca <luciannamend@gmail.com>
2024-08-12 17:32:12 -0400

    Added message to know when app starts

--- src/main/java/com/example/financewatchapi/FinanceWatchApiApplication.java
+++ src/main/java/com/example/financewatchapi/FinanceWatchApiApplication.java
@@ -3,11 +3,16 @@ package com.example.financewatchapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class FinanceWatchApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(FinanceWatchApiApplication.class, args);
-	}
 
+		System.out.println("\n============================================================");
+		System.out.println("             Finance Watch Api Started");
+		System.out.println("============================================================\n");
+	}
 }
